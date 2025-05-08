1b45ba344b2c162e94fa682e2a59b32aa8730df5
akitanabraham <161664811+akitanabraham@users.noreply.github.com>
2025-05-08 10:05:59 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -2,12 +2,12 @@
 
 
 # Demande à l'utilisateur d'entrer un nom de fichier
-echo "Entrez le nom du fichier :"
+echo "Entrez le nom du fichier. :"
 read filename
 
 # La condition [ -f filename ] vérifie si le fichier existe et est un fichier régulier
 if [ -f "$filename" ]; then
-    echo "Le fichier '$filename' existe"
+    echo "Le fichier '$filename' existe."
 else
-    echo "Le fichier '$filename' n'existe pas"
+    echo "Le fichier '$filename' n'existe pas."
 fi
